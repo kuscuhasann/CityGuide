9e236251882e375e43c1ccd1059c6c70cad59cfd
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-02-20 13:57:25 +0300

    Project Created & Start
    This project using Asp.Net Web API system.

---
+++ CityGuide.API/WebApplication1/App_Start/BundleConfig.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Web
+Imports System.Web.Optimization
+
+Public Module BundleConfig
+    ' Paketleme hakkında daha fazla bilgi için lütfen https://go.microsoft.com/fwlink/?LinkId=301862 adresini ziyaret edin
+    Public Sub RegisterBundles(ByVal bundles As BundleCollection)
+        bundles.Add(New ScriptBundle("~/bundles/jquery").Include(
+                   "~/Scripts/jquery-{version}.js"))
+
+        ' Geliştirme yapmak ve öğrenme kaynağı olarak yararlanmak için Modernizr uygulamasının geliştirme sürümünü kullanın. Ardından
+        ' üretim için hazır. https://modernizr.com adresinde derleme aracını kullanarak yalnızca ihtiyacınız olan testleri seçin.
+        bundles.Add(New ScriptBundle("~/bundles/modernizr").Include(
+                    "~/Scripts/modernizr-*"))
+
+        bundles.Add(New ScriptBundle("~/bundles/bootstrap").Include(
+                    "~/Scripts/bootstrap.js"))
+
+        bundles.Add(New StyleBundle("~/Content/css").Include(
+                    "~/Content/bootstrap.css",
+                    "~/Content/site.css"))
+    End Sub
+End Module
