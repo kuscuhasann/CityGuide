a1234fba4cf0ab0b549fd8be4cfb3ae5ab19424e
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-03-10 15:31:03 +0300

    Remastered City Guide Backend
    Added Datebase connections and db injections.
    Added Data class and data class's databases connections.

---
+++ CityGuide.API/CityGuide.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityGuide.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+    }
+}
