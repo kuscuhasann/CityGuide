2db16433eb238e4d357f95c5e269688f97373e42
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-03-14 02:57:15 +0300

    Finished Dto class desing.

--- CityGuide.API/CityGuide.API/Dtos/CityForListDto.cs
+++ CityGuide.API/CityGuide.API/Dtos/CityForListDto.cs
@@ -9,7 +9,7 @@ namespace CityGuide.API.Dtos
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string  Url { get; set; }
+        public string  PhotoUrl { get; set; }
         public string Description { get; set; }
     }
 }
